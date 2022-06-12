cee7aedd6a097b5869ef3aae603431e2d5fdae03
Luca Terracciano <terraccianoluca@gmail.com>
2022-06-12 03:03:47 +0200

    integrate data cleaning in gen data script

--- clean_data.py
+++ clean_data.py
@@ -1,5 +1,4 @@
 import sys
-import re
 
 raw_data_path = sys.argv[1]
 raw_data = ''
@@ -7,6 +6,6 @@ raw_data = ''
 with open(raw_data_path, 'r') as f:
     raw_data = f.read()
 clean_data = raw_data.replace('|\n', '\n')
-with open('try.txt', 'w') as f:
+with open(raw_data_path, 'w') as f:
     f.write(clean_data)
 
--- gen_tables.sh
+++ gen_tables.sh
@@ -11,3 +11,16 @@ $1 -vf -s $2 -T n
 $1 -vf -s $2 -T L 
 $1 -vf -s $2 -T l 
 $1 -vf -s $2 -T c
+
+mkdir ./db_data
+
+cp *.tbl ./db_data
+
+python clean_data.py ./db_data/customer.tbl
+python clean_data.py ./db_data/lineitem.tbl
+python clean_data.py ./db_data/nation.tbl
+python clean_data.py ./db_data/orders.tbl
+python clean_data.py ./db_data/part.tbl
+python clean_data.py ./db_data/partsupp.tbl
+python clean_data.py ./db_data/region.tbl
+python clean_data.py ./db_data/supplier.tbl
