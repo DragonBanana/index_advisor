cc9bbffd3a7f2de4ccf6ced038bd5a8fe54ffaec
Luca Terracciano <terraccianoluca@gmail.com>
2022-06-12 02:36:22 +0200

    gen_tables script

---
+++ gen_tables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+dbgen -vf -s $1 -T s
+dbgen -vf -s $1 -T S 
+dbgen -vf -s $1 -T r 
+dbgen -vf -s $1 -T P 
+dbgen -vf -s $1 -T p 
+dbgen -vf -s $1 -T O 
+dbgen -vf -s $1 -T o 
+dbgen -vf -s $1 -T n 
+dbgen -vf -s $1 -T L 
+dbgen -vf -s $1 -T l 
+dbgen -vf -s $1 -T c
