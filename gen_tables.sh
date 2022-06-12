860e82aa9479082528f6a8f55b92309d6933a5e2
Luca Terracciano <terraccianoluca@gmail.com>
2022-06-12 03:09:34 +0200

    integrate data copy data

--- gen_tables.sh
+++ gen_tables.sh
@@ -24,3 +24,13 @@ python clean_data.py ./db_data/part.tbl
 python clean_data.py ./db_data/partsupp.tbl
 python clean_data.py ./db_data/region.tbl
 python clean_data.py ./db_data/supplier.tbl
+
+
+psql -U adv -h 127.0.0.1 -c "COPY customer FROM './db_data/customer.tbl' DELIMITERS '|' CSV;"
+psql -U adv -h 127.0.0.1 -c "COPY customer FROM './db_data/lineitem.tbl' DELIMITERS '|' CSV;"
+psql -U adv -h 127.0.0.1 -c "COPY customer FROM './db_data/nation.tbl' DELIMITERS '|' CSV;"
+psql -U adv -h 127.0.0.1 -c "COPY customer FROM './db_data/orders.tbl' DELIMITERS '|' CSV;"
+psql -U adv -h 127.0.0.1 -c "COPY customer FROM './db_data/part.tbl' DELIMITERS '|' CSV;"
+psql -U adv -h 127.0.0.1 -c "COPY customer FROM './db_data/partsupp.tbl' DELIMITERS '|' CSV;"
+psql -U adv -h 127.0.0.1 -c "COPY customer FROM './db_data/region.tbl' DELIMITERS '|' CSV;"
+psql -U adv -h 127.0.0.1 -c "COPY customer FROM './db_data/supplier.tbl' DELIMITERS '|' CSV;"
